8bcb05f314931f34037462b9183330482569cbeb
zastrowm <zastrowm+sourcecontrol@gmail.com>
2011-04-14 03:26:50 +0000

    Updated StringWrite withe comments

--- stdlib/StringWriter.h
+++ stdlib/StringWriter.h
@@ -2,48 +2,83 @@
 #pragma once
 
 #include "inc.h"
+#include "string.h"
 #include "StringBuffer.h"
 
 namespace atmega{
-
+	
+	/**
+	 *	Generic class for allowing a class to use << as string appending/writing
+	 *
+	 *	Use:
+	 *
+	 *	class Buffer : StringWriter<Buffer>;
+	 *
+	 *	Buffer b;
+	 *	String friend("John");
+	 *	b<<"Hello "<<friend<<endl;
+	 */
 	template<typename TYPE>
 	class StringWriter{
 
 	public:
 		StringWriter(){}
 
+		/**
+		 *	Write a single letter
+		 *	
+		 *	@param let the letter to write
+		 *	@return itself to allow for more StringWriting
+		 */
 		StringWriter& operator<<(char let){
 			((TYPE*)(this))->put(let);
 			return *this;
 		}
 		
+		/**
+		 *	Write a string literal
+		 *	
+		 *	@param str the string literal to write
+		 *	@return itself to allow for more StringWriting
+		 */
 		StringWriter& operator<<(char *str){
 			((TYPE*)(this))->put(str);
 			return *this;
 		}
-
+		
+		/**
+		 *	Write a string
+		 *	
+		 *	@param str the string to write
+		 *	@return itself to allow for more StringWriting
+		 */
 		StringWriter& operator<<(string &str) {
 			((TYPE*)(this))->put(str);
 			return *this;
 		}
 
+		/**
+		 *	Write a newline
+		 *	
+		 *	@param ed unused
+		 *	@return itself to allow for more StringWriting
+		 */
 		StringWriter& operator<<(_endl ed){
 			((TYPE*)(this))->put('\r');
 			((TYPE*)(this))->put('\n');
 			return *this;
 		}
-		
+
+		/**
+		 *	Write a tab
+		 *	
+		 *	@param ed [unused]
+		 *	@return itself to allow for more StringWriting
+		 */		
 		StringWriter& operator<<(_tab ed){
 			((TYPE*)(this))->put('\t');
 			return *this;
 		}
-		
-
 
 	};
-
-
-	
-
-
 }
