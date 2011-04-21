56792e5e9c250a2d41842842acd637720cdcc760
zastrowm <zastrowm+sourcecontrol@gmail.com>
2011-04-21 04:41:41 +0000

    Added putHex, putNum to StringWriter

--- stdlib/StringWriter.h
+++ stdlib/StringWriter.h
@@ -9,10 +9,13 @@
  *	Allow objects to use << for sending strings/characters to the object.  Put() must be defined for
  *		characters, strings, and literal strings
  */
-#define INHERIT_STREAM(T_TYPE)	T_TYPE& operator<<(char let){put(let);return *this;}			\
-								T_TYPE& operator<<(string s){put(s);return *this;}				\
-								T_TYPE& operator<<(char *str){put(str);return *this;}			\
-								T_TYPE& operator<<(_endl ed){put('\r');put('\n');return *this;}	\
-								T_TYPE& operator<<(_tab ed){put('\t');return *this;}			\
-								T_TYPE& operator<<(StringBuffer<128> sb){string t; t = sb.toString();put(t);return *this;}
+#define INHERIT_STREAM(T_TYPE)	T_TYPE& operator<<(char let){put(let);return *this;}												\
+								T_TYPE& operator<<(string s){put(s);return *this;}													\
+								T_TYPE& operator<<(char *str){put(str);return *this;}												\
+								T_TYPE& operator<<(_endl ed){put('\r');put('\n');return *this;}										\
+								T_TYPE& operator<<(_tab ed){put('\t');return *this;}												\
+								T_TYPE& operator<<(StringBuffer<128> sb){string t; t = sb.toString();put(t);return *this;}			\
+								T_TYPE& putHex(uint8_t value){char buf[16]; sprintf(buf,"%x",value); put(buf) ;return *this;};			\
+								T_TYPE& putNum(uint8_t value){char buf[16]; sprintf(buf,"%d",value); put(buf) ;return *this;};			\
+								T_TYPE& putNum(uint16_t value){char buf[16]; sprintf(buf,"%d",value); put(buf) ;return *this;};
 									
