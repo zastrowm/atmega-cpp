a4a22f5153c0e6056dfe15ef8b5471529adb0d98
zastrowm <zastrowm@d98dba3d-bc33-33af-a0d3-3ddf204cb995>
2011-04-21 04:31:01 +0000

    Various Little Changes to files

--- trunk/main.c
+++ trunk/main.c
@@ -17,6 +17,7 @@
 #include "atmega/serial.h"
 #include "atmega/twi.h"
 #include "atmega/ImageInfo.h"
+#include "atmega/util.h"
 
 USING_CPP();
 USING_ATMEGA();
@@ -48,15 +49,10 @@ ISR(INT2_vect) {
 	sei();
 }
 
-uint8_t readIO(uint8_t port){
-	return *((volatile uint8_t*)(port + 0x20));
-}
-
-void writeIO(uint8_t port,uint8_t value){
-	*((volatile uint8_t*)(port + 0x20)) = value;
-}
-
 void printHelp(Serial &serial);
+bool handleCommand(string cmd, uint8_t count, uint8_t arg1, uint8_t arg2);
+
+static const uint8_t CAM_ADDRESS = 0xC0;
 
 int main(){
 	cli();
@@ -67,7 +63,7 @@ int main(){
 	Serial serial;
 	
 	servo::init();
-	static const uint8_t CAM_ADDRESS = 0xC0;
+	
 	TwoWireInterface::init();
 	
 	serial << "Booting..." << endl;
@@ -78,90 +74,94 @@ int main(){
 		buffer.recalculateIndex();
 		cmd = buffer.toString();
 		
-		bool handled = false;
-		
-		switch(count){		
-		case 1:	//just the command
-			if (cmd % "reset"){
-				servo::reset();
-				handled = true;
-			} else if (cmd % "help"){
-				printHelp(serial);
-				handled = true;
-			} else if (cmd % "startCam" || cmd % "sac") {
-				DDRA = 0x00;
-				sei();
-				uint8_t buf = MCUCR;
-				// set int1 trigger on any change
-				buf &= ~(1 << ISC11);
-				buf |= (1 << ISC10);
-				
-				// set int0 trigger on any change
-				buf &= ~(1 << ISC01);
-				buf |= (1 << ISC00);
-				
-				MCUCR |= buf;
-				
-				// one for rising edge zero for falling edge on INT2
-				MCUSR |= (1 << ISC2);
-				
-				// enable all three interrupts
-				GICR = (1 << INT1) | (1 << INT0) | (1 << INT2);
-
-				
-				serial << "Port A: " << PORTA + 'A' << endl;
-				serial << "global buf: " << globalBuf << endl;
-				handled = true;
-			} else if (cmd % "stopCam" ||
-						cmd % "soc") {
-				cli();
-				handled = true;
-			}			
-			break;
-		case 2:	//command and 1 arg
-			if (cmd % "tilt"){
-				servo::setTilt(servo::mapTilt(arg1));
-				handled = true;
-			} else if (cmd % "pan"){
-				servo::setPan(servo::mapPan(arg1));
-				handled = true;
-			}  else if (cmd % "readIO"){
-				gBuffer.sprintf("Port 0x%x = 0x%x", arg1, readIO(arg1));
-				serial<<gBuffer<<endl;				
-				handled = true;
-			} else if (cmd % "camRegRead" || cmd % "crr") {
-				uint8_t data = TwoWireInterface::read(CAM_ADDRESS, arg1);
-				if (TwoWireInterface::error) {
-					serial << "Two wire interface error." << endl;
-				} else {
-					gBuffer.sprintf("Camera register 0x%x = 0x%x", arg1, data);
-					serial << gBuffer << endl;
-				}
-				
-				handled = true;
-			}
-			break;
-		case 3:	//command and 2 args
-			if (cmd % "writeIO") {
-				writeIO(arg1,arg2);
-				handled = true;
-			} else if (cmd % "camRegWrite" || cmd % "crw") {
-				TwoWireInterface::write(CAM_ADDRESS, arg1, arg2);
-				
-				if (TwoWireInterface::error)	serial << "Two wire interface error." << endl;					
-				 else							serial << "Two wire write success." << endl;
-				
-				handled = true;
-			}
-		}
-		
-		if (!handled){
+		if (!handleCommand(cmd,count,arg1,arg2)){
 			buffer.sprintf("Unrecognized Command: %s",request.str());
 			serial<<buffer.str()<<endl;
 			printHelp(serial);	
 		}
 	}
 	return 0;
+}
+
+/**
+ *	Handle a command string
+ *	
+ *	@param cmd the cmd string to handle
+ *	@param count the number of arguments
+ *	@param arg1 the first argument to the command
+ *	@param arg2 the second argument to the command
+ *	@return whether or not the command was handled
+ */
+bool handleCommand(string cmd, uint8_t count, uint8_t arg1, uint8_t arg2){
+	
+	switch(count){		
+	case 1:	//just the command
+		if (cmd % "reset"){
+			servo::reset();
+		} else if (cmd % "help"){
+			printHelp(serial);
+		} else if (cmd % "startCam" || cmd % "sac") {
+			DDRA = 0x00;
+			sei();
+			uint8_t buf = MCUCR;
+			// set int1 trigger on any change
+			buf &= ~(1 << ISC11);
+			buf |= (1 << ISC10);
+				
+			// set int0 trigger on any change
+			buf &= ~(1 << ISC01);
+			buf |= (1 << ISC00);
+				
+			MCUCR |= buf;
+				
+			// one for rising edge zero for falling edge on INT2
+			MCUSR |= (1 << ISC2);
+				
+			// enable all three interrupts
+			GICR = (1 << INT1) | (1 << INT0) | (1 << INT2);
+
+				
+			serial << "Port A: " << PORTA + 'A' << endl;
+			serial << "global buf: " << globalBuf << endl;
+		} else if (cmd % "stopCam" || cmd % "soc") {
+			cli();
+		} else  return false;
+		
+		break;
+	case 2:	//command and 1 arg
+		if (cmd % "tilt"){
+			servo::setTilt(arg1);
+		} else if (cmd % "pan"){
+			servo::setPan(arg1);
+		}  else if (cmd % "readIO"){
+			gBuffer.sprintf("Port 0x%x = 0x%x", arg1, readIO(arg1));
+			serial<<gBuffer<<endl;				
+		} else if (cmd % "camRegRead" || cmd % "crr") {
+			uint8_t data = TwoWireInterface::read(CAM_ADDRESS, arg1);
+			if (TwoWireInterface::error) {
+				serial << "Two wire interface error." << endl;
+			} else {
+				gBuffer.sprintf("Camera register 0x%x = 0x%x", arg1, data);
+				serial << gBuffer << endl;
+			}
+		} else return false;
+		break;
+	case 3:	//command and 2 args
+		if (cmd % "writeIO") {
+			writeIO(arg1,arg2);
+		} else if (cmd % "camRegWrite" || cmd % "crw") {
+			TwoWireInterface::write(CAM_ADDRESS, arg1, arg2);
+				
+			if (TwoWireInterface::error)	serial << "Two wire interface error." << endl;					
+			else							serial << "Two wire write success." << endl;
+		} else return false;
+		break;
+	default:
+		return false;
+	}
+		
+	//if we made it this far, we had a success:
+	return true;
 }
 
 /**
--- trunk/stdlib/inc.h
+++ trunk/stdlib/inc.h
@@ -5,40 +5,33 @@
 #define F_CPU 16e6
 #endif
 
+
+
+#define USING_CPP() void * operator new(size_t size) {return malloc(size);}										\
+					void operator delete(void * ptr) { free(ptr); } 											\
+					template <typename TSIZE> atmega::_stringnode<TSIZE> atmega::_stringnode<TSIZE>::nullnode;	\
+					template <typename TSIZE> atmega::BasicString<TSIZE> atmega::BasicString<TSIZE>::empty;		\
+					namespace atmega{_endl endl;}																\
+					namespace atmega{_tab tab;}																	\
+					namespace atmega{_commit commit;}															\
+					namespace atmega{StringBuffer<128> gBuffer;}
+					//uint8_t TwoWireInterface::error = false;						
+
+
 namespace atmega{
 	struct _endl{};
 	struct _tab{};
 	struct _commit{};
-}
-
-
-#define _DEFINE_GLOBALS_	namespace atmega{StringBuffer<128> gBuffer;};
-
-
-
-#define USING_CPP() template <typename TSIZE> atmega::_stringnode<TSIZE> atmega::_stringnode<TSIZE>::nullnode;	\
-					template <typename TSIZE> atmega::BasicString<TSIZE> atmega::BasicString<TSIZE>::empty;		\
-					void * operator new(size_t size) {return malloc(size);}										\
-					void operator delete(void * ptr) { free(ptr); } 											\
-					namespace atmega{_endl endl;}																\
-					namespace atmega{_tab tab;}																	\
-					namespace atmega{_commit commit;}															\
-					_DEFINE_GLOBALS_
-					//uint8_t TwoWireInterface::error = false;						
-
-
-
-
-namespace atmega{
+		
 	extern _endl endl;
 	extern _tab tab;
 	extern _commit commit;
 };
 
-#ifdef SDFS
+
 void * operator new(size_t size); 
 void operator delete(void * ptr); 
-#endif
+
 
 #define  EEPROM      __attribute__((section(".eeprom")))
 #define  IN_FLASH    __attribute__((progmem))
